4246a7fe72f0bb1971e1eca59ff314acf417c551
Aditya Kumar Jha <163677863+AdityaCypherX@users.noreply.github.com>
2026-02-12 00:45:09 +0530

    Create Q5_solution.py

---
+++ 01_Python_Fundamentals/Q5_solution.py
@@ -0,0 +1,12 @@
+# ---------------------------------------------
+# Q5: Expression Evaluation
+# Problem:
+# Evaluate and print the result of the expression:
+# x = 10 + 3 * 2 ** 2
+# Explain why the output is what it is based on operator precedence.
+# ---------------------------------------------
+
+x = 10 + 3 * 2 ** 2
+print("Result of expression:", x)
+print("Explanation: Exponent > Multiplication > Addition")
+print("-" * 50)
