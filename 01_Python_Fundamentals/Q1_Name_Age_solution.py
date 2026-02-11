7933a60347924b8d9dd6269d6c14f9fc0f6142e8
Aditya Kumar Jha <163677863+AdityaCypherX@users.noreply.github.com>
2026-02-11 23:40:27 +0530

    Q1_solution.py

---
+++ 01_Python_Fundamentals/Q1_Name_Age_solution.py
@@ -0,0 +1,8 @@
+ Problem:
+# Write a program that asks the user for their name and age,
+# then prints:
+# "Hello <name>, you are <age> years old!"
+
+name=input("enter your name")
+age=int(input("enter your age"))
+print("hello",name,"you are",age,"years old")
