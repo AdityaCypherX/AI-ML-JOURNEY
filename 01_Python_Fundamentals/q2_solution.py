7a8cf3e0a441ac1db74ef03fe194ba69ad0733fb
Aditya Kumar Jha <163677863+AdityaCypherX@users.noreply.github.com>
2026-02-12 00:09:20 +0530

    Create q2_solution.py

---
+++ 01_Python_Fundamentals/q2_solution.py
@@ -0,0 +1,21 @@
+Q2: Arithmetic Operations
+# Problem:
+# Take two numbers as input from the user and print their:
+# sum, difference, product, and quotient.
+# ---------------------------------------------
+
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+sum=num1+num2
+difference=num1-num2
+product=num1*num2
+quotient=num1/num2
+#print(f"sum of {num1} and {num2} is {sum}")
+#print(f"difference of {num1} and {num2} is {difference}")
+#print(f"product of {num1} and {num2} is {product}")
+#print(f"quotient of {num1} and {num2} is {quotient}")
+
+print("sum is:",sum)
+print("difference is:",difference)
+print("product is:",product)
+print("quotient is:",quotient)
