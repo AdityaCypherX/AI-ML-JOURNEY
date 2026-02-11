725e185455d09df30c3c12fb08d99eae176e7874
Aditya Kumar Jha <163677863+AdityaCypherX@users.noreply.github.com>
2026-02-12 00:50:51 +0530

    Create Q8_solution.py

---
+++ 01_Python_Fundamentals/Q8_solution.py
@@ -0,0 +1,15 @@
+# ---------------------------------------------
+# Q8: Area of Circle
+# Problem:
+# Take the radius (r) as user input and print the area of a circle.
+#
+# Formula:
+# Area = π * r^2   (Use π = 3.14)
+# ---------------------------------------------
+
+
+# Q8: Area of circle
+r = float(input("Enter radius: "))
+pi = 3.14
+area = pi * r * r
+print("Area of circle:", area)
