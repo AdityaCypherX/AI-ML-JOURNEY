06295e3c4d75005e9a756336ad21e70074393390
Aditya Kumar Jha <163677863+AdityaCypherX@users.noreply.github.com>
2026-02-12 00:49:08 +0530

    Create Q7_solution.py

---
+++ 01_Python_Fundamentals/Q7_solution.py
@@ -0,0 +1,13 @@
+# ---------------------------------------------
+# Q7: Celsius to Fahrenheit Conversion
+# Problem:
+# Ask the user for a temperature in Celsius (string input),
+# convert it to float, and calculate temperature in Fahrenheit.
+#
+# Formula:
+# Fahrenheit = (Celsius * (9/5)) + 32
+# ---------------------------------------------
+# Q7: Celsius to Fahrenheit
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * (9/5)) + 32
+print("Temperature in Fahrenheit:", fahrenheit)
