325c85f2c9603c183f8e515b63134855c85b7b7a
Aditya Kumar Jha <163677863+AdityaCypherX@users.noreply.github.com>
2026-02-12 00:18:12 +0530

    Create Q3_solution.py

---
+++ 01_Python_Fundamentals/Q3_solution.py
@@ -0,0 +1,15 @@
+# ---------------------------------------------
+# Q3: Average Calculation
+# Problem:
+# Ask the user to enter two integers and one float.
+# Convert them all to floats and print their average.
+# ---------------------------------------------
+
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+num3=float(input("enter the third number:"))
+num1=float(num1)
+num2=float(num2)
+num3=float(num3)
+num4=(num1+num2+num3)/3
+print("average of three num is:",num4)
