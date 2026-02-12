de211187345322d60a033a49cb172ad8a4581400
Aditya Kumar Jha <163677863+AdityaCypherX@users.noreply.github.com>
2026-02-12 23:45:01 +0530

    Create Q2_even_numbers.py

---
+++ 02_python_fundamental_assignment/Q2_even_numbers.py
@@ -0,0 +1,16 @@
+"""
+Assignment: Python Fundamentals - Assignment 02
+Question No: Q2
+Topic: Functions, Loops
+Author: Aditya Jha
+Description:
+Function to print all even numbers between two numbers (inclusive).
+"""
+def print_even(a, b):
+    for i in range(a, b + 1):
+        if i % 2 == 0:
+            print(i)
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print_even(a, b)
